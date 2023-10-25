a83513c43430d730e0462cf4a141efff4095e56d
Pratap <pratap.careers@gmail.com>
2023-10-25 21:39:35 +0530

    fixed dark mode

--- src/components/darkmode/DarkMode.jsx
+++ src/components/darkmode/DarkMode.jsx
@@ -26,9 +26,11 @@ function DarkMode() {
     if (dark) {
       root.style.setProperty("--clr-background", "#242424");
       root.style.setProperty("--clr", "#9b9b9b");
+      root.style.setProperty("--box-shadow", "4px 4px 15px #0000008f");
     } else {
       root.style.setProperty("--clr-background", "#fffff7");
       root.style.setProperty("--clr", "#545454");
+      root.style.setProperty("--box-shadow", "0 13px 30px #0000001c");
     }
   }, [dark]);
   return (
